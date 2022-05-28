ad194720ed186f4343cd1881ff650712b7e08a3b
Shanks lll <shanksx00@gmail.com>
2022-05-28 10:56:38 +0200

    FicheFrais controller

---
+++ MVC_API_2/Controllers/FicheFraisController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using classes_metier;
+using System.Web.Http.Cors;
+using System.Linq;
+using classes_outils;
+using System;
+using System.Net.Http;
+using System.Net;
+
+namespace Test_1.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class FicheFraisController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetAllFraisForfaits()
+        {
+            try
+            {
+                CfraisForfaits oFraisForfaits = CfraisForfaits.getInstance();
+                if (oFraisForfaits.oListFraisForfaits != null)
+                {
+                    return Json(oFraisForfaits.oListFraisForfaits);
+                }
+                return BadRequest("ERREUR");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAllLigneFF()
+        {
+            try
+            {
+                CligneFFs oLigneFFs = CligneFFs.getInstance();
+                if (oLigneFFs.oListLigneFFs != null)
+                {
+                    return Json(oLigneFFs.oListLigneFFs);
+                }
+                return BadRequest("ERREUR");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAllLigneFHF()
+        {
+            try
+            {
+                CligneFHFs oLigneFHs = CligneFHFs.getInstance();
+                if (oLigneFHs.oListLigneFHFs != null)
+                {
+                    return Json(oLigneFHs.oListLigneFHFs);
+                }
+                return BadRequest("ERREUR");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
